cacf85ef980cac728ba32fd1a3df93607a96a760
Gauri3005 <129527142+Gauri3005@users.noreply.github.com>
2023-04-11 15:54:35 +0530

    Update array_1.java

--- array_java/src/Array/array_1.java
+++ array_java/src/Array/array_1.java
@@ -4,12 +4,13 @@ public class array_1 {
 	public static void main(String[] args) {
 		
 		int a[]= {10,20,30,40,50};
-		String ar[]=new String[5];
+		String ar[]=new String[6];
 		ar[0]="Revansh";
 		ar[1]="Mrunal";
 		ar[2]="Shlok";
 		ar[3]="Manasvi";
 		ar[4]="Raya";
+		ar[5]="Arush";  //manager did changes
 		for(int i=0;i<=4;i++) {
 			System.out.println(ar[i]);
 			}
